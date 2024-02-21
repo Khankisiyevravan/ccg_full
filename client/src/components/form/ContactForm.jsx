01bd00394e716a53d86687a5019d15bf9bfbf31d
Khankisiyevravan <revan.xankisiyev.02@mail.ru>
2024-02-21 14:21:35 +0400

    update

--- client/src/components/form/ContactForm.jsx
+++ client/src/components/form/ContactForm.jsx
@@ -9,6 +9,7 @@ const Contact = () => {
     content: "",
   });
   console.log(process.env.REACT_APP_API_URL);
+  
   // const axiosInstance = axios.create({
   //   baseURL: process.env.REACT_APP_API_URL,
   // });
